4d392612671999a175a45729c8c1e59784d923ca
Misael Alberto <misael.alberto@outlook.com>
2024-05-13 17:22:01 -0300

    style: Adciiona o pio css

--- src/main.tsx
+++ src/main.tsx
@@ -22,7 +22,9 @@ enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
-        <App />      
+        <div style={{ margin: '0 auto', maxWidth: '768px'}}>
+          <App /> 
+        </div>             
       </QueryClientProvider>      
     </React.StrictMode>,
   )    
