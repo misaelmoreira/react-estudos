b7feb77885e7f2e83c0ac85d3c0dce2f7e739dc2
Misael Alberto <misael.alberto@outlook.com>
2024-05-13 08:23:24 -0300

    Criar botao para alterar as tarefas

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 # Iniciando aplicacao 
 npm run-script dev
+
+# verificar o que os elementos pode receber de parametros
+interface dom 
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,42 @@
-import { useState } from "react"
+import React, { useState } from "react"
 
-const tarefas = [
-  {
-    id: 1,
-    nome: "Estudar React",
-    concluida: false
-  },
-  {
-    id: 2,
-    nome: "Estudar Typescript",
-    concluida: false 
-  }
-]
 
+// Pode fazer com type e interface
+type AcaoProps = {
+  concluida?: boolean
+} & React.ComponentProps<'button'>
+
+function Acao({ concluida, ...props }: AcaoProps) {
+  return <button {...props}>{concluida ? 'YES' : 'NO'}</button>
+}
 
 function App() {
-  const { tarefas, setTarefas } = useState;
+  const [ tarefas, setTarefas ] = useState([
+    {
+      id: 1,
+      nome: "Estudar React",
+      concluida: false
+    },
+    {
+      id: 2,
+      nome: "Estudar Typescript",
+      concluida: false 
+    }    
+  ])
+
+  const marcarComoConcluida = (id: number) => {
+    setTarefas(tarefas.map(tarefa => {
+      if(tarefa.id === id){
+        return {
+          ...tarefa,
+          concluida: !tarefa.concluida
+        }
+      }
+      return tarefa
+    }))
+
+  }
+
   return (
     <div>
       <h1>Tarefas</h1>
@@ -23,8 +44,13 @@ function App() {
       <ul>
         {tarefas.map(tarefa => (
           <li key={tarefa.id}>
-            {tarefa.nome}
-            {tarefa.concluida ? ' YES' : ' NO'}
+            {tarefa.nome} 
+            <Acao 
+              concluida={tarefa.concluida} 
+              onClick={() => {
+                marcarComoConcluida(tarefa.id)
+              }}
+            />           
           </li>
         ))}
       </ul>
