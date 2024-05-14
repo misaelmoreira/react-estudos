e02016f82f865187a844c9af30dfe02a2fe1e8c5
Misael Alberto <misael.alberto@outlook.com>
2024-05-14 14:54:01 -0300

    chore: inserindo o arquivo de teste utils

---
+++ src/utils/test-utils.tsx
@@ -0,0 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { render as testingLibraryRender, RenderOptions } from "@testing-library/react";
+import React from "react";
+
+export { default as userEvent } from '@testing-library/user-event'
+
+export * from '@testing-library/react'
+
+export const render = (
+    ui: React.ReactElement,
+    {...renderOptions}: RenderOptions = {}
+) => testingLibraryRender(
+    <QueryClientProvider client={new QueryClient()}>{ui}</QueryClientProvider>,
+    renderOptions
+)
