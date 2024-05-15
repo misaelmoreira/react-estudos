42cd41a4a95843b3f6bff48888c7e11145aeae3f
Misael Alberto <misael.alberto@outlook.com>
2024-05-15 09:15:34 -0300

    chore: adiciona o eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true, jest: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
@@ -8,7 +8,17 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+    'eslint-plugin-import-helpers',
+    'eslint-plugin-jest',
+    'eslint-plugin-storybook',
+    'testing-library',
+    'react-refresh',
+
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
