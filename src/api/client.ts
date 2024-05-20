9bdd1ecde55ab91fefdc0641dd836cb0459d5ad5
Misael Alberto <misael.alberto@outlook.com>
2024-05-20 08:13:03 -0300

    feat: caso o token esteja presente, e passado no cabeçalho da requisição

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,9 +1,18 @@
 import axios from 'axios'
 
+const token = localStorage.getItem('token') || ''
+
+const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': ''
+}
+
+if (token) {
+    headers.Authorization = `Bearer ${token}`
+}   
+
 
 export const client = axios.create({
     baseURL: 'http://localhost:3000',
-    headers: {
-        'Content-Type': 'application/json'
-    }
+    headers
 })
