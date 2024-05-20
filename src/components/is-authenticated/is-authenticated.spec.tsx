a6e188ddaa6a9ed65cf16dde13c325d0e1353390
Misael Alberto <misael.alberto@outlook.com>
2024-05-20 09:26:20 -0300

    feat(isAuthenticated): cria componente que exige autenticação do usuário

---
+++ src/components/is-authenticated/is-authenticated.spec.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "../../utils/test-utils";
+import { IsAuthenticated } from "./is-authenticated";
+
+vi.mock('react-router-dom', () => ({
+    Navigate: ({ to }: { to: string }) => <div data-testid="navigate">{to}</div>,
+    Outlet: () => <div data-testid="outlet"></div>
+}));
+
+
+describe("<IsAuthenticated />", () => {
+  it("deve redirecionar o usuário para a página de login quando ele não estiver autenticado", async () => {
+
+    render(<IsAuthenticated />);   
+
+    expect(screen.getByTestId('navigate')).toHaveTextContent('/login');
+  })  
+
+
+  it("deve renderizar os componentes filhos", async () => {
+    global.localStorage.setItem('token', 'mock_token');
+
+    render(<IsAuthenticated />);   
+
+    expect(screen.getByTestId('outlet')).toBeInTheDocument();
+    global.localStorage.removeItem('token');
+  })
+});
