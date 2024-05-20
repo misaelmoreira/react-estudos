63a861502130a7a92b2566f5e3a8c6daf86eb9e9
Misael Alberto <misael.alberto@outlook.com>
2024-05-20 15:27:05 -0300

    chore: adiciona um exemplo simples de context API

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,10 +1,48 @@
+import { FC, createContext, useContext } from "react";
 import { Heading } from "../../components/heading";
 
-export function Dashboard() {
+type MyContext = {
+  nome: string;
+  idade: number;
+  cidade: string;
+}
+
+const MyContext = createContext({} as MyContext)
+
+const useMyContext = () => useContext(MyContext)
+
+const Nome: FC = () => {
+  const { nome } = useMyContext()
+  return <p>{nome}</p>
+}
+
+const Idade: FC = () => {
+  const { idade } = useMyContext()
+  return <p>{idade}</p>
+}
+
+const Cidade: FC = () => {
+  const { cidade } = useMyContext()
+  return <p>{cidade}</p>
+}
+
+export const Dashboard = () => {
+  const info = {
+    nome: "João",
+    idade: 20,
+    cidade: "São Paulo"
+  }
 
   return (
-    <Heading>
-      Dashboard
-    </Heading>
+    <>
+      <Heading>
+        Dashboard
+      </Heading>
+      <MyContext.Provider value={info}>
+        <Nome />
+        <Idade />
+        <Cidade />
+      </MyContext.Provider>
+    </>    
   );
 }
